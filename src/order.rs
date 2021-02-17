ad7bb27e42d553d84bcd2e3c84114f0d2d50edc9
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2021-02-17 04:13:34 +0100

    ♻️ refactor(order): remove 'where' clauses to shorten visit methods

--- src/order.rs
+++ src/order.rs
@@ -2,7 +2,6 @@ use rust_decimal::{prelude::FromPrimitive, Decimal};
 use rusty_money::{iso, Money};
 use serde::de::{self};
 use serde::{Deserialize, Deserializer};
-use std::error::Error;
 use std::fmt;
 
 use crate::status::Status;
@@ -26,23 +25,17 @@ impl<'de> de::Visitor<'de> for DeserializeISO4217MoneyVisitor {
         formatter.write_str("TODO")
     }
 
-    fn visit_f64<E>(self, v: f64) -> Result<Self::Value, E>
-    where
-        E: Error,
+    fn visit_f64<Error>(self, v: f64) -> Result<Self::Value, Error>
     {
         Ok(Money::from_decimal(Decimal::from_f64(v).unwrap(), iso::EUR))
     }
 
-    fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
-    where
-        E: Error,
+    fn visit_i64<Error>(self, v: i64) -> Result<Self::Value, Error>
     {
         Ok(Money::from_decimal(Decimal::from_i64(v).unwrap(), iso::EUR))
     }
 
-    fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
-    where
-        E: Error,
+    fn visit_u64<Error>(self, v: u64) -> Result<Self::Value, Error>
     {
         Ok(Money::from_decimal(Decimal::from_u64(v).unwrap(), iso::EUR))
     }
