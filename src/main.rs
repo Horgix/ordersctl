72dcc8375bd101504e831d0b892a8021723b9ee5
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2025-08-28 18:37:58 +0200

    feat: take orders file path from ORDERSCTL_ORDERS_FILE env var

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 #[macro_use]
 extern crate prettytable;
 
@@ -6,8 +8,9 @@ pub mod orders;
 pub mod status;
 
 fn main() {
+    let orders_file = env::var("ORDERSCTL_ORDERS_FILE").unwrap_or("./samples/orders.yml".to_string());
 
-    let orders = orders::read_orders_from_file("./samples/actual-horgix-orders.yml");
+    let orders = orders::read_orders_from_file(orders_file);
     match orders {
         Ok(orders) => println!("Successfully read orders from file: \n{}", orders),
         Err(e) => println!("Error reading orders: {}", e),
