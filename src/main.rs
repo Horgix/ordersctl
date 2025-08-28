4a6267b547c9c0e5835338e964eb7f78c9126fe4
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2025-08-28 20:51:54 +0200

    feat: serve an actual CLI with --help using clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use clap::{Parser, Subcommand};
 
 #[macro_use]
 extern crate prettytable;
@@ -7,13 +8,42 @@ pub mod order;
 pub mod orders;
 pub mod status;
 
+/// Main CLI structure
+#[derive(Parser, Debug)]
+#[command(
+    name = "ordersctl",
+    version = "0.1",
+    author = "Alexis Horgix Chotard <ordersctl@foss.horgix.fr>",
+    about = " OrdersCtl — A simple CLI to track your orders",
+)]
+struct OrdersCtlCli {
+    /// Subcommands
+    #[command(subcommand)]
+    command: Option<OrdersCtlSubcommands>,
+}
+
+/// Top-level subcommands
+#[derive(Subcommand, Debug)]
+enum OrdersCtlSubcommands {
+    /// List all recordings (and their transcription statuses TODO)
+    List {},
+}
+
 fn main() {
     let orders_file =
         env::var("ORDERSCTL_ORDERS_FILE").unwrap_or("./samples/orders.yml".to_string());
 
     let orders = orders::read_orders_from_file(orders_file);
-    match orders {
-        Ok(orders) => println!("Successfully read orders from file: \n{}", orders),
-        Err(e) => println!("Error reading orders: {}", e),
+
+
+    let cli = OrdersCtlCli::parse();
+
+    match &cli.command.unwrap_or(OrdersCtlSubcommands::List {}) {
+        OrdersCtlSubcommands::List {} => {
+            match orders {
+                Ok(orders) => println!("Successfully read orders from file: \n{}", orders),
+                Err(e) => println!("Error reading orders: {}", e),
+            }
+        }
     }
 }
